baac68035da0b980f28b2d774305695ad6490de7
brigoni <brian.rigoni1@gmail.com>
2019-11-07 21:33:26 -0300

    algoritmo de nextfit funcionando -- todos los algoritmos de seleccion funcionan --

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from models import (Task, MemoryManager, BestFit, NextFit,
 PATH_TO_FILE = './datasets/dataset1.txt'
 # Campos para instanciar el administrador de memoria
 tasks = []
-selection_algorithm = FirstFit() # Cambiar por cualquier estrategia de seleccion
-selection_time = 1
-assignation_time = 1
-release_time = 1
+selection_algorithm = NextFit() # Cambiar por cualquier estrategia de seleccion
+selection_time = 0
+assignation_time = 0
+release_time = 0
 memory_qty = 150
 
 
--- models/selection_algorithms/nextfit.py
+++ models/selection_algorithms/nextfit.py
@@ -2,17 +2,25 @@ from .iselectionalgorithm import ISelectionAlgorithm
 
 
 class NextFit(ISelectionAlgorithm):
-    pointer = 0
 
     def __init__(self):
-        pass
+        self.pointer = 0
 
     def get_partition(self, task, memory):
         print('NextFit ejecutando seleccion de particion')
-        for partition in memory.partitions[self.pointer:]:
+        iterations = 0
+        print('Puntero de nextfit se encuentra en ', memory.partitions[self.pointer])
+        while iterations < len(memory.partitions):
+            partition = memory.partitions[self.pointer]
             if (partition.task is None) and (partition.space_assigned >= task.space_requested):
                 print('NextFit encontro lugar')
                 return partition
+            # Reset del idx para recorrer particiones
+            if self.pointer == len(memory.partitions)-1:
+                self.pointer = 0
+            else:
+                self.pointer += 1
+            iterations+=1
         print('NextFit no fue capaz de encontrar particion')
         return None
 
